a71dd6d4b03ed4bfdea5ebc2a8bb14457cc3908d
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-02-11 16:50:39 +0200

    Yes

---
+++ js/number.js
@@ -0,0 +1,40 @@
+const totalPrice = 100;
+const discount = totalPrice * 0.10;
+const result = totalPrice - discount;
+console.log(result);
+
+const kalving = 50;
+const faranngeyt = (kalving - 273.15) * 1.8 + 32.00;
+console.log(faranngeyt)
+
+const daysInMonth = 30;
+const hoursInMonth = daysInMonth * 24;
+const minutesInMonth = hoursInMonth * 1440;
+console.log(daysInMonth);
+console.log(hoursInMonth);
+console.log(minutesInMonth);
+
+let health = 100;
+health -= 30;
+console.log(health);
+let energy = 80;
+energy -= 57;
+console.log(energy);
+
+const floatNumber = 12.78 ;
+console.log(Math.floor(floatNumber));
+
+const floatString = "45.67";
+const parsedFloat = (parseFloat(floatString))
+console.log(parsedFloat);
+
+const intString = "123";
+const parsedIntта = Number(parseInt(intString));
+console.log(typeof(parsedIntта));
+console.log(parsedIntта);
+
+const sqrtNumber = 25;
+console.log(Math.sqrt(sqrtNumber));
+
+const integer = 42;
+const stringNumber = "256";
