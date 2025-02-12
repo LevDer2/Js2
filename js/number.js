7618b50977b5586d5b67d05913298213f245a68d
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-02-12 12:13:19 +0200

    new

--- js/number.js
+++ js/number.js
@@ -1,30 +1,30 @@
 const totalPrice = 100;
 const discount = totalPrice * 0.10;
 const result = totalPrice - discount;
-console.log(result);
+console.log("Price in discount:", result);
 
-const kalving = 50;
-const faranngeyt = (kalving - 273.15) * 1.8 + 32.00;
-console.log(faranngeyt)
+const celsius = 20;
+const faranngeyt = (celsius * (9/5)) + 32;
+console.log("Faranngeyt:", faranngeyt);
 
 const daysInMonth = 30;
 const hoursInMonth = daysInMonth * 24;
 const minutesInMonth = hoursInMonth * 1440;
-console.log(daysInMonth);
-console.log(hoursInMonth);
-console.log(minutesInMonth);
+console.log("daysInMonth", daysInMonth);
+console.log("hoursInMonth", hoursInMonth);
+console.log("minutesInMonth", minutesInMonth);
 
 let health = 100;
 health -= 30;
-console.log(health);
+console.log("Health:", health);
 let energy = 80;
 energy -= 57;
-console.log(energy);
+console.log("Energy:", energy);
 
 const floatNumber = 12.78 ;
 console.log(Math.floor(floatNumber));
 
-const floatString = "45.67";
+const floatString = "45.67px";
 const parsedFloat = (parseFloat(floatString))
 console.log(parsedFloat);
 
@@ -36,5 +36,6 @@ console.log(parsedIntта);
 const sqrtNumber = 25;
 console.log(Math.sqrt(sqrtNumber));
 
-const integer = 42;
-const stringNumber = "256";
+const chus = "15px";
+console.log(Number.parseInt(chus));
+console.log(chus.toString(chus));
